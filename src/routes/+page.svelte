b56c38a4e05073f3b5876a286b6e55e14f0ebf1f
Travis Luckenbaugh <tluckenbaugh@gmail.com>
2023-10-31 22:07:12 -0400

    Start list

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Card Games</h1>
+
+<!-- <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p> -->
+
+<h2>One Deck</h2>
+<ul>
+    <li><a href="/hearts">Hearts</a></li>
+    <li>Spades</li>
+    <li>Crazy Eights</li>
+    <li>Kings in the Corner</li>
+    <li>Rummy 500</li>
+    <li>Euchre</li>
+</ul>
+
+<h2>Two Decks</h2>
+<ul>
+    <li>Gin Rummy</li>
+    <li>Bridge</li>
+</ul>
---
+++ src/routes/hearts/+page.svelte
@@ -0,0 +1,3 @@
+<a href="/">Back to list</a>
+
+<h1>Hearts</h1>
