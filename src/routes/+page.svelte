157208776d1e6348851937ce6cc05ef9dee1738a
Travis Luckenbaugh <tluckenbaugh@gmail.com>
2023-11-02 21:01:54 -0400

    Setup game list

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,137 @@
 <h1>Card Games</h1>
 
-<!-- <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p> -->
-
-<h2>One Deck</h2>
-<ul>
-    <li><a href="/hearts">Hearts</a></li>
-    <li>Spades</li>
-    <li>Crazy Eights</li>
-    <li>Kings in the Corner</li>
-    <li>Rummy 500</li>
-    <li>Euchre</li>
-</ul>
-
-<h2>Two Decks</h2>
-<ul>
-    <li>Gin Rummy</li>
-    <li>Bridge</li>
-</ul>
+<table>
+    <tr>
+        <th>Game</th>
+        <th>Type</th>
+        <th>Difficult</th>
+        <th>Players</th>
+        <th>Decks</th>
+    </tr>
+    <tr>
+        <td><a href="https://bicyclecards.com/how-to-play/go-fish">Go Fish</a></td>
+        <td>Other</td>
+        <td>Easy</td>
+        <td>2-5+</td>
+        <td>1</td>
+    </tr>
+    <tr>
+        <td><a href="https://bicyclecards.com/how-to-play/gin-rummy">Gin Rummy</a></td>
+        <td>Rummy</td>
+        <td>Medium</td>
+        <td>2</td>
+        <td>1</td>
+    </tr>
+    <tr>
+        <td><a href="https://bicyclecards.com/how-to-play/rummy-rum">Rummy</a></td>
+        <td>Rummy</td>
+        <td>Medium</td>
+        <td>2-5+</td>
+        <td>1</td>
+    </tr>
+    <tr>
+        <td><a href="https://bicyclecards.com/how-to-play/crazy-eights">Crazy Eights</a></td>
+        <td>Other</td>
+        <td>Easy</td>
+        <td>2-5+</td>
+        <td>1</td>
+    </tr>
+    <tr>
+        <td><a href="https://bicyclecards.com/how-to-play/spades">Spades</a></td>
+        <td>Trick Taking</td>
+        <td>Medium</td>
+        <td>2-5+</td>
+        <td>1</td>
+    </tr>
+    <tr>
+        <td><a href="https://bicyclecards.com/how-to-play/war">War</a></td>
+        <td>Other</td>
+        <td>Easy</td>
+        <td>2</td>
+        <td>1</td>
+    </tr>
+    <tr>
+        <td><a href="https://bicyclecards.com/how-to-play/hearts">Hearts</a></td>
+        <td>Trick Taking</td>
+        <td>Medium</td>
+        <td>2-5+</td>
+        <td>1</td>
+    </tr>
+    <tr>
+        <td><a href="https://bicyclecards.com/how-to-play/kings-corner">King's Corner</a></td>
+        <td>Solitaire</td>
+        <td>Medium</td>
+        <td>2-4</td>
+        <td>1</td>
+    </tr>
+    <tr>
+        <td><a href="https://bicyclecards.com/how-to-play/solitaire">Solitaire</a></td>
+        <td>Solitaire</td>
+        <td>Medium</td>
+        <td>1</td>
+        <td>1</td>
+    </tr>
+    <tr>
+        <td><a href="https://bicyclecards.com/how-to-play/six-card-golf">Six Card Golf</a></td>
+        <td>Other</td>
+        <td>Easy</td>
+        <td>2-4</td>
+        <td>1</td>
+    </tr>
+    <tr>
+        <td><a href="https://bicyclecards.com/how-to-play/euchre">Euchre</a></td>
+        <td>Trick Taking</td>
+        <td>Medium</td>
+        <td>2,4</td>
+        <td>1</td>
+    </tr>
+    <tr>
+        <td><a href="https://bicyclecards.com/how-to-play/old-maid">Old Maid</a></td>
+        <td>Other</td>
+        <td>Easy</td>
+        <td>2-5+</td>
+        <td>1</td>
+    </tr>
+    <tr>
+        <td><a href="https://bicyclecards.com/how-to-play/500-rum">500 Rummy</a></td>
+        <td>Rummy</td>
+        <td>Medium</td>
+        <td>2-5+</td>
+        <td>1</td>
+    </tr>
+    <tr>
+        <td><a href="https://bicyclecards.com/how-to-play/canasta">Canasta</a></td>
+        <td>Rummy</td>
+        <td>Medium</td>
+        <td>4</td>
+        <td>1</td>
+    </tr>
+    <tr>
+        <td><a href="https://bicyclecards.com/how-to-play/palace">Palace</a></td>
+        <td>Other</td>
+        <td>Medium</td>
+        <td>2-5</td>
+        <td>1-2</td>
+    </tr>
+    <tr>
+        <td><a href="https://bicyclecards.com/how-to-play/bridge">Bridge</a></td>
+        <td>Trick Taking</td>
+        <td>Hard</td>
+        <td>4</td>
+        <td>2</td>
+    </tr>
+    <tr>
+        <td><a href="https://bicyclecards.com/how-to-play/spider-solitaire">Spider Solitaire</a></td>
+        <td>Solitaire</td>
+        <td>Medium</td>
+        <td>1</td>
+        <td>2</td>
+    </tr>
+    <tr>
+        <td><a href="https://bicyclecards.com/how-to-play/cribbage">Cribbage</a></td>
+        <td>Other</td>
+        <td>Hard</td>
+        <td>2-3</td>
+        <td>1</td>
+    </tr>
+</table>
